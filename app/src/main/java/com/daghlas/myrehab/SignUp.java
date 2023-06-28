c9a6eeb95d35a3143fc8028af2eea71d85367286
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-06-28 21:16:16 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/SignUp.java
+++ app/src/main/java/com/daghlas/myrehab/SignUp.java
@@ -1,11 +1,20 @@
 package com.daghlas.myrehab;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    Button signUp;
+    TextView login_here, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,45 @@ public class SignUp extends AppCompatActivity {
         //status bar color
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.green));
 
+        signUp = findViewById(R.id.signup_btn);
+        login_here = findViewById(R.id.loginS);
+        cancel = findViewById(R.id.cancelS);
+
+        //go to main activity when log in button is clicked
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(SignUp.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //go to main activity when log in button is clicked
+        login_here.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //go to main activity when log in button is clicked
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    //go back to log in activity when back button is pressed
+    public void onBackPressed() {
+        Intent intent = new Intent(SignUp.this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
