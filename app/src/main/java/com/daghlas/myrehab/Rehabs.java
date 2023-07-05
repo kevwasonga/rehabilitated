da8a4c49dd2fa58b85b6c8cd12b1bf01e20fa21f
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-05 10:32:33 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/Rehabs.java
+++ app/src/main/java/com/daghlas/myrehab/Rehabs.java
@@ -1,5 +1,6 @@
 package com.daghlas.myrehab;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -8,18 +9,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Rehabs extends AppCompatActivity {
 
     Button save;
+    TextView name1, name3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rehabs);
 
+        nameIdEmail();
+
         save = findViewById(R.id.save);
+        name1 = findViewById(R.id.fname);
+        name3 = findViewById(R.id.lname);
 
         //status bar color
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.green));
@@ -61,5 +75,22 @@ public class Rehabs extends AppCompatActivity {
         startActivity(intent);
         finish();
         super.onBackPressed();
+    }
+
+    public void nameIdEmail() {
+        //database
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userID = mAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = firebaseFirestore.collection("USERS").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                assert value != null;
+                name1.setText(value.getString("firstName"));
+                name3.setText(value.getString("lastName"));
+            }
+        });
     }
 }
