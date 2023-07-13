9be2bf932e26981897674b3ffa87e56a1d71cf26
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-13 08:59:22 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/Login.java
+++ app/src/main/java/com/daghlas/myrehab/Login.java
@@ -1,10 +1,8 @@
 package com.daghlas.myrehab;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,10 +12,16 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * DEVELOPER: daghlaskaire58@gmail.com
+ * For KELVIN WASONGA
+ * JULY 2023
+ */
 
 public class Login extends AppCompatActivity {
 
@@ -41,65 +45,56 @@ public class Login extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         //Firebase
-        mAuth= FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        autoLogin();
 
         //status bar color
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.green));
 
         //go to main activity when log in button is clicked
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!validateEmail() | !validatePwd()){
-                    progressBar.setVisibility(View.GONE);
-                    login.setVisibility(View.VISIBLE);
-                }else{
-                    logIN();
-                }
+        login.setOnClickListener(v -> {
+            if (!validateEmail() | !validatePwd()) {
+                progressBar.setVisibility(View.GONE);
+                login.setVisibility(View.VISIBLE);
+            } else {
+                logIN();
             }
         });
 
         //sign up activity
-        register_account.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, SignUp.class);
-                startActivity(intent);
-                finish();
-            }
+        register_account.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, SignUp.class);
+            startActivity(intent);
+            finish();
         });
 
         //reset password activity
-        reset_password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Login.this, "Contact admin", Toast.LENGTH_SHORT).show();
-            }
-        });
+        reset_password.setOnClickListener(v -> Toast.makeText(Login.this, "Contact admin", Toast.LENGTH_SHORT).show());
     }
 
-    public boolean validateEmail(){
+    public boolean validateEmail() {
         String val = email.getText().toString().trim();
-        if(val.isEmpty()){
+        if (val.isEmpty()) {
             email.setError("Enter email");
             return false;
-        }else {
+        } else {
             email.setError(null);
             return true;
         }
     }
-    public boolean validatePwd(){
+
+    public boolean validatePwd() {
         String val = password.getText().toString().trim();
-        if(val.isEmpty()){
+        if (val.isEmpty()) {
             password.setError("Enter password");
             return false;
-        }else {
+        } else {
             password.setError(null);
             return true;
         }
     }
 
-    public void logIN(){
+    public void logIN() {
         progressBar.setVisibility(View.VISIBLE);
         login.setVisibility(View.GONE);
         //logging in
@@ -107,21 +102,40 @@ public class Login extends AppCompatActivity {
         String passwordS = password.getText().toString().trim();
 
         mAuth.signInWithEmailAndPassword(emailS, passwordS)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Intent intent = new Intent(Login.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
-                        }else {
-                            Log.w("Login Error:", task.getException());
-                            Toast.makeText(Login.this, "wrong email/password", Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                            login.setVisibility(View.VISIBLE);
-                        }
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //SAVE CREDENTIALS
+                        SharedPreferences sharedPreferences = getSharedPreferences("sessionManager", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("email", emailS);
+                        editor.putString("password", passwordS);
+                        editor.apply();
+                        //END SAVE
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.w("Login Error:", task.getException());
+                        Toast.makeText(Login.this, "wrong email/password", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        login.setVisibility(View.VISIBLE);
                     }
                 });
+    }
+
+    public void autoLogin() {
+        //retrieve values from stored preference
+        SharedPreferences sharedPreferencesLogin = getSharedPreferences("sessionManager", Context.MODE_PRIVATE);
+        //read values from stored preference
+        String emailPref = sharedPreferencesLogin.getString("email", "");
+        String passPref = sharedPreferencesLogin.getString("password", "");
+
+        if (!emailPref.isEmpty() && !passPref.isEmpty()) {
+            // User is already logged in, redirect on Main activity
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
