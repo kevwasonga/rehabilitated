2efba95ee9d668f28fe510fbdf6cfd9f2dc5c282
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-13 08:59:10 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/RManagement.java
+++ app/src/main/java/com/daghlas/myrehab/RManagement.java
@@ -1,31 +1,43 @@
 package com.daghlas.myrehab;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DEVELOPER: daghlaskaire58@gmail.com
+ * For KELVIN WASONGA
+ * JULY 2023
+ */
 
 public class RManagement extends AppCompatActivity implements RManagementInterface {
 
     RecyclerView recyclerView;
-    TextView name1, name3;
+    TextView name1, name3, text, text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +54,48 @@ public class RManagement extends AppCompatActivity implements RManagementInterfa
         nameIdEmail();
         name1 = findViewById(R.id.fname);
         name3 = findViewById(R.id.lname);
+        text = findViewById(R.id.text);
+        text2 = findViewById(R.id.text2);
+
+        text2.setOnClickListener(v -> {
+            Intent intent = new Intent(RManagement.this, Rehabs.class);
+            startActivity(intent);
+            finish();
+        });
 
         //inflating the adapter to the recyclerview
         recyclerView = findViewById(R.id.recyclerView);
-        RManagementAdapter adapter = new RManagementAdapter(this, this);
+        List<RModel> rModelList = new ArrayList<>();
+        RManagementAdapter adapter = new RManagementAdapter(this, this, rModelList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("rehabs");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    RModel rModel = dataSnapshot.getValue(RModel.class);
+                    rModelList.add(rModel);
+
+                    if (adapter.getItemCount() == 0) {
+                        recyclerView.setVisibility(View.INVISIBLE);
+                        text.setVisibility(View.VISIBLE);
+                        text2.setVisibility(View.VISIBLE);
+                    } else {
+                        recyclerView.setVisibility(View.VISIBLE);
+                        text.setVisibility(View.INVISIBLE);
+                        text2.setVisibility(View.INVISIBLE);
+                    }
+                }adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
     @Override
     public boolean onSupportNavigateUp() {
@@ -63,6 +111,7 @@ public class RManagement extends AppCompatActivity implements RManagementInterfa
         super.onBackPressed();
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -79,6 +128,8 @@ public class RManagement extends AppCompatActivity implements RManagementInterfa
         return super.onOptionsItemSelected(item);
     }
 
+ */
+
     @Override
     public void onItemClick(int position) {
         Intent intent = new Intent(RManagement.this, SetRehab.class);
@@ -93,13 +144,10 @@ public class RManagement extends AppCompatActivity implements RManagementInterfa
         String userID = mAuth.getCurrentUser().getUid();
 
         DocumentReference documentReference = firebaseFirestore.collection("USERS").document(userID);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                assert value != null;
-                name1.setText(value.getString("firstName"));
-                name3.setText(value.getString("lastName"));
-            }
+        documentReference.addSnapshotListener(this, (value, error) -> {
+            assert value != null;
+            name1.setText(value.getString("firstName"));
+            name3.setText(value.getString("lastName"));
         });
     }
 }
