df412234e6abf45723089c0193e39c4699e85fca
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-17 11:11:54 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/KManagement.java
+++ app/src/main/java/com/daghlas/myrehab/KManagement.java
@@ -1,31 +1,49 @@
 package com.daghlas.myrehab;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DEVELOPER: daghlaskaire58@gmail.com
+ * For KELVIN WASONGA
+ * JULY 2023
+ */
+
 public class KManagement extends AppCompatActivity implements KManagementInterface {
 
     RecyclerView recyclerView;
-    TextView name1, name3;
+    TextView name1, name3, text, text2;
+    KManagementAdapter adapter;
+    List<KModel> kModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +52,7 @@ public class KManagement extends AppCompatActivity implements KManagementInterfa
 
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.green));
 
-        if(getSupportActionBar()!=null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Manage Enrolled Kids");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
@@ -42,14 +60,54 @@ public class KManagement extends AppCompatActivity implements KManagementInterfa
         nameIdEmail();
         name1 = findViewById(R.id.fname);
         name3 = findViewById(R.id.lname);
+        text = findViewById(R.id.text);
+        text2 = findViewById(R.id.text2);
+
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KManagement.this, Enrollment.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         //inflating the adapter to the recyclerview
         recyclerView = findViewById(R.id.recyclerView);
-        KManagementAdapter adapter = new KManagementAdapter(this,this);
+        kModelList = new ArrayList<>();
+        adapter = new KManagementAdapter(this, this, kModelList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("enrolled");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    KModel kModel = dataSnapshot.getValue(KModel.class);
+                    kModelList.add(kModel);
+
+                    if (adapter.getItemCount() == 0) {
+                        recyclerView.setVisibility(View.INVISIBLE);
+                        text.setVisibility(View.VISIBLE);
+                        text2.setVisibility(View.VISIBLE);
+                    } else {
+                        recyclerView.setVisibility(View.VISIBLE);
+                        text.setVisibility(View.INVISIBLE);
+                        text2.setVisibility(View.INVISIBLE);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
@@ -71,21 +129,31 @@ public class KManagement extends AppCompatActivity implements KManagementInterfa
         finish();
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.search) {
-            Toast.makeText(this, "button to search kids on this list..", Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+ */
 
     public void nameIdEmail() {
         //database
