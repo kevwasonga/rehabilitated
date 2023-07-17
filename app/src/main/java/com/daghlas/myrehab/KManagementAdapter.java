06cf06430768688c8ca815bfef5418cc543a66df
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-17 11:12:27 +0300

    initial commit

--- app/src/main/java/com/daghlas/myrehab/KManagementAdapter.java
+++ app/src/main/java/com/daghlas/myrehab/KManagementAdapter.java
@@ -1,22 +1,42 @@
 package com.daghlas.myrehab;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class KManagementAdapter extends RecyclerView.Adapter<KManagementAdapter.MyViewHolder> {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DEVELOPER: daghlaskaire58@gmail.com
+ * For KELVIN WASONGA
+ * JULY 2023
+ */
+
+public class KManagementAdapter extends RecyclerView.Adapter<KManagementAdapter.MyViewHolder> implements Filterable {
 
     private final KManagementInterface kManagementInterface;
+    private final List<KModel> kModelList;
+    //search
+    private final List<KModel> kModelListFull;
     Context context;
-    public KManagementAdapter(Context context, KManagementInterface kManagementInterface){
+    public KManagementAdapter(Context context, KManagementInterface kManagementInterface, List<KModel> kModelList){
         this.context = context;
         this.kManagementInterface = kManagementInterface;
+        this.kModelList = kModelList;
+        //search
+        kModelListFull = new ArrayList<>(kModelList);
     }
 
     @NonNull
@@ -31,13 +51,67 @@ public class KManagementAdapter extends RecyclerView.Adapter<KManagementAdapter.
     @Override
     public void onBindViewHolder(@NonNull KManagementAdapter.MyViewHolder holder, int position) {
         //binding recyclerview rows according to position
+        KModel kModel = kModelList.get(position);
+
+        holder.name.setText(kModel.getFullName());
+        holder.rehab.setText(kModel.getRehab());
+        holder.date.setText(kModel.getDateEnrolled());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, SetKid.class);
+            intent.putExtra("fullName", kModelList.get(holder.getAdapterPosition()).getFullName());
+            intent.putExtra("rehab", kModelList.get(holder.getAdapterPosition()).getRehab());
+            intent.putExtra("date", kModelList.get(holder.getAdapterPosition()).getDateEnrolled());
+            intent.putExtra("residence", kModelList.get(holder.getAdapterPosition()).getResidence());
+            intent.putExtra("guardian", kModelList.get(holder.getAdapterPosition()).getGuardian());
+            intent.putExtra("gender", kModelList.get(holder.getAdapterPosition()).getGender());
+            intent.putExtra("health", kModelList.get(holder.getAdapterPosition()).getHealth());
+            intent.putExtra("fingerprint", kModelList.get(holder.getAdapterPosition()).getFingerprint());
+            intent.putExtra("faceID", kModelList.get(holder.getAdapterPosition()).getFaceID());
+            context.startActivity(intent);
+            ((Activity)context).finish();
+        });
     }
 
     @Override
     public int getItemCount() {
         //returns number of rows in recyclerview
-        return 15;
+        return kModelList.size();
     }
+
+    //search
+    @Override
+    public Filter getFilter() {
+        return kModelFilter;
+    }
+    private final Filter kModelFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<KModel> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0){
+                filteredList.addAll(kModelListFull);
+            }else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (KModel item : kModelListFull){
+                    if(item.getFullName().toLowerCase().contains(filterPattern)){
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            kModelList.clear();
+            kModelList.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+    //search end
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
@@ -51,14 +125,11 @@ public class KManagementAdapter extends RecyclerView.Adapter<KManagementAdapter.
             date = itemView.findViewById(R.id.date_enrolled);
             profile = itemView.findViewById(R.id.profile_pic);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(kManagementInterface != null){
-                        int pos = getAdapterPosition();
-                        if(pos != RecyclerView.NO_POSITION){
-                            kManagementInterface.onItemClick(pos);
-                        }
+            itemView.setOnClickListener(v -> {
+                if(kManagementInterface != null){
+                    int pos = getAdapterPosition();
+                    if(pos != RecyclerView.NO_POSITION){
+                        kManagementInterface.onItemClick(pos);
                     }
                 }
             });
